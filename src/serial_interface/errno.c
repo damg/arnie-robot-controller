8f2b75c19786e06869596bbfed3b3c72cdee5e48
Dmitri Bachtin <damg.dev@gmail.com>
2008-12-12 16:32:57 +0100

    Added additional IO errors

--- src/serial_interface/errno.c
+++ src/serial_interface/errno.c
@@ -14,6 +14,14 @@ const char* ar_io_errstring(AR_IO_ERRNO_T e)
       return "Error reading from connection";
     case AR_IO_EWRITE:
       return "Error writing to connection";
+    case AR_IO_ERESET:
+      return "Error resetting device";
+    case AR_IO_EINVBAUD:
+      return "Requested invalid baud rate";
+    case AR_IO_ESETBAUD:
+      return "Failed to set requested baud rate";
+    case AR_IO_ECLOSE:
+      return "Failed to close connection";
     default:
       return "Unknown IO error";
     }
--- src/serial_interface/errno.h
+++ src/serial_interface/errno.h
@@ -4,8 +4,12 @@
 typedef enum {
   AR_IO_EINIT,
   AR_IO_EOPEN,
+  AR_IO_ERESET,
   AR_IO_EREAD,
   AR_IO_EWRITE,
+  AR_IO_EINVBAUD,
+  AR_IO_ESETBAUD,
+  AR_IO_ECLOSE,
 } AR_IO_ERRNO_T;
 
 extern AR_IO_ERRNO_T ar_io_errno;
