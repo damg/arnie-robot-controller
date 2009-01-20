a2800456fcfffa91238e32e071e4893ec95d8251
Dmitri Bachtin <dima2001@toaster.localdomain>
2009-01-20 14:17:21 +0100

    Initialization parameters for the arnie robot

---
+++ src/model/arnie_params.h
@@ -0,0 +1,174 @@
+#ifndef __AR_M_ARNIE_PARAMS_H__
+#define __AR_M_ARNIE_PARAMS_H__
+
+// Left foot
+// Join 1
+#define _AR_M_DH_ll_j1_a = 0.0;
+#define _AR_M_DH_ll_j1_d = 0.0;
+#define _AR_M_DH_ll_j1_alpha = M_PI_2;
+#define _AR_M_DH_ll_j1_theta_off = 0.0;
+#define _AR_M_DH_ll_j1_theta_min = -0.34906585;
+#define _AR_M_DH_ll_j1_theta_max = 2.583087293;
+#define _AR_M_DH_ll_j1_mass = 0.0; // 92.7215300;
+#define _AR_M_DH_ll_j1_com_x = 0.0; // -4.4234198;
+#define _AR_M_DH_ll_j1_com_y = 0.0; // -14.0188990;
+#define _AR_M_DH_ll_j1_com_z = 0.0; // 0.3597805;
+
+// Join 2
+#define _AR_M_DH_ll_j2_a = 10.9982;
+#define _AR_M_DH_ll_j2_d = 44.85;
+#define _AR_M_DH_ll_j2_alpha = - M_PI_2;
+#define _AR_M_DH_ll_j2_theta_off = - M_PI_2;
+#define _AR_M_DH_ll_j2_theta_min = -M_PI_5_6th;
+#define _AR_M_DH_ll_j2_theta_max = M_PI_5_6th;
+#define _AR_M_DH_ll_j2_mass = 0.0; // 15.3023720;
+#define _AR_M_DH_ll_j2_com_x = 0.0; // 1.2132379;
+#define _AR_M_DH_ll_j2_com_y = 0.0; // -6.4567850;
+#define _AR_M_DH_ll_j2_com_z = 0.0; // 11.8765470;
+
+// Join 3
+#define _AR_M_DH_ll_j3_a = 91;
+#define _AR_M_DH_ll_j3_d = 0.0;
+#define _AR_M_DH_ll_j3_alpha = 0.0;
+#define _AR_M_DH_ll_j3_theta_off = - M_PI_2;
+#define _AR_M_DH_ll_j3_theta_min = -1.623156204;
+#define _AR_M_DH_ll_j3_theta_max = 2.181661565;
+#define _AR_M_DH_ll_j3_mass = 0.0; // 164.5129700;
+#define _AR_M_DH_ll_j3_com_x = 0.0; // 14.9749620;
+#define _AR_M_DH_ll_j3_com_y = 0.0; // -45.5000000;
+#define _AR_M_DH_ll_j3_com_z = 0.0; // 0.0000000;
+
+// Join 4
+#define _AR_M_DH_ll_j4_a = sqrt(11629.0);
+#define _AR_M_DH_ll_j4_d = 0.0;
+#define _AR_M_DH_ll_j4_alpha = 0.0;
+#define _AR_M_DH_ll_j4_theta_off = atan(35.0 / 102.0);
+#define _AR_M_DH_ll_j4_theta_min = -2.234021443;
+#define _AR_M_DH_ll_j4_theta_max = 1.500983157;
+#define _AR_M_DH_ll_j4_mass = 0.0; // 29.8616050;
+#define _AR_M_DH_ll_j4_com_x = 0.0; // 4.5686886;
+#define _AR_M_DH_ll_j4_com_y = 0.0; // -43.2604700;
+#define _AR_M_DH_ll_j4_com_z = 0.0; // -1.7063145;
+
+// Join 5
+#define _AR_M_DH_ll_j5_a = 0.0;
+#define _AR_M_DH_ll_j5_d = 0.0;
+#define _AR_M_DH_ll_j5_alpha = M_PI_2;
+#define _AR_M_DH_ll_j5_theta_off = - atan(35.0 / 102.0);
+#define _AR_M_DH_ll_j5_theta_min = -M_PI_5_6th; // -0.872664626;
+#define _AR_M_DH_ll_j5_theta_max = M_PI_5_6th; // 2.199114858;
+#define _AR_M_DH_ll_j5_mass = 0.0; // 86.5480690;
+#define _AR_M_DH_ll_j5_com_x = 0.0; // -2.7180418;
+#define _AR_M_DH_ll_j5_com_y = 0.0; // 13.5969630;
+#define _AR_M_DH_ll_j5_com_z = 0.0; // -5.2014226;
+
+// Join 6
+#define _AR_M_DH_ll_j6_a = 31.0;
+#define _AR_M_DH_ll_j6_d = 40.25;
+#define _AR_M_DH_ll_j6_alpha = 0.0;
+#define _AR_M_DH_ll_j6_theta_off = 0.0;
+#define _AR_M_DH_ll_j6_theta_min = - M_PI_2;
+#define _AR_M_DH_ll_j6_theta_max = M_PI_2;
+#define _AR_M_DH_ll_j6_mass = 0.0; // 179.8308000;
+#define _AR_M_DH_ll_j6_com_x = 0.0; // 2.8558632;
+#define _AR_M_DH_ll_j6_com_y = 0.0; // -17.3958130;
+#define _AR_M_DH_ll_j6_com_z = 0.0; // 0.1439557;
+
+// Join 7 (fixed)
+#define _AR_M_DH_ll_j7_a = 0.0;
+#define _AR_M_DH_ll_j7_d = 0.0;
+#define _AR_M_DH_ll_j7_alpha = 0.0;
+#define _AR_M_DH_ll_j7_theta_off = M_PI_2;
+
+// Right foot
+// Join 1
+#define _AR_M_DH_rl_j1_a = 0.0;
+#define _AR_M_DH_rl_j1_d = 0.0;
+#define _AR_M_DH_rl_j1_alpha = M_PI_2;
+#define _AR_M_DH_rl_j1_theta_off = 0.0;
+#define _AR_M_DH_rl_j1_theta_min = -2.583087293;
+#define _AR_M_DH_rl_j1_theta_max = 0.34906585;
+#define _AR_M_DH_rl_j1_mass = 0.0; // 92.7215300;
+#define _AR_M_DH_rl_j1_com_x = 0.0; // -4.4234198;
+#define _AR_M_DH_rl_j1_com_y = 0.0; // 14.0188990;
+#define _AR_M_DH_rl_j1_com_z = 0.0; // 0.3597805;
+
+// Join 2
+#define _AR_M_DH_rl_j2_a = 10.9982;
+#define _AR_M_DH_rl_j2_d = 44.85;
+#define _AR_M_DH_rl_j2_alpha = - M_PI_2;
+#define _AR_M_DH_rl_j2_theta_off = - M_PI_2;
+#define _AR_M_DH_rl_j2_theta_min = -M_PI_5_6th;
+#define _AR_M_DH_rl_j2_theta_max = M_PI_5_6th;
+#define _AR_M_DH_rl_j2_mass = 0.0; // 15.3023720;
+#define _AR_M_DH_rl_j2_com_x = 0.0; // -1.2132379;
+#define _AR_M_DH_rl_j2_com_y = 0.0; // -6.4567850;
+#define _AR_M_DH_rl_j2_com_z = 0.0; // 11.8765470;
+
+// Join 3
+#define _AR_M_DH_rl_j3_a = 91;
+#define _AR_M_DH_rl_j3_d = 0.0;
+#define _AR_M_DH_rl_j3_alpha = 0.0;
+#define _AR_M_DH_rl_j3_theta_off = - M_PI_2;
+#define _AR_M_DH_rl_j3_theta_min = -1.623156204;
+#define _AR_M_DH_rl_j3_theta_max = 2.181661565;
+#define _AR_M_DH_rl_j3_mass = 0.0; // 164.5129700;
+#define _AR_M_DH_rl_j3_com_x = 0.0; // -14.9749620;
+#define _AR_M_DH_rl_j3_com_y = 0.0; // -45.5000000;
+#define _AR_M_DH_rl_j3_com_z = 0.0; // 0.0000000;
+
+// Join 4
+#define _AR_M_DH_rl_j4_a = sqrt(11629.0);
+#define _AR_M_DH_rl_j4_d = 0.0;
+#define _AR_M_DH_rl_j4_alpha = 0.0;
+#define _AR_M_DH_rl_j4_theta_off = atan(35.0 / 102.0);
+#define _AR_M_DH_rl_j4_theta_min = -2.234021443;
+#define _AR_M_DH_rl_j4_theta_max =  1.500983157;
+#define _AR_M_DH_rl_j4_mass = 0.0; // 29.8616050;
+#define _AR_M_DH_rl_j4_com_x = 0.0; // -4.5686886;
+#define _AR_M_DH_rl_j4_com_y = 0.0; // -43.2604700;
+#define _AR_M_DH_rl_j4_com_z = 0.0; // -1.7063145;
+
+// Join 5
+#define _AR_M_DH_rl_j5_a = 0.0;
+#define _AR_M_DH_rl_j5_d = 0.0;
+#define _AR_M_DH_rl_j5_alpha = M_PI_2;
+#define _AR_M_DH_rl_j5_theta_off = - atan(35.0 / 102.0);
+#define _AR_M_DH_rl_j5_theta_min = -M_PI_5_6th; // -0.872664626;
+#define _AR_M_DH_rl_j5_theta_max = M_PI_5_6th; // 2.199114858;
+#define _AR_M_DH_rl_j5_mass = 0.0; // 86.5480690;
+#define _AR_M_DH_rl_j5_com_x = 0.0; // -2.7180418;
+#define _AR_M_DH_rl_j5_com_y = 0.0; // 13.5969630;
+#define _AR_M_DH_rl_j5_com_z = 0.0; // 5.2014226;
+
+// Join 6
+#define _AR_M_DH_rl_j6_a = 31.0;
+#define _AR_M_DH_rl_j6_d = 40.25;
+#define _AR_M_DH_rl_j6_alpha = 0.0;
+#define _AR_M_DH_rl_j6_theta_off = 0.0;
+#define _AR_M_DH_rl_j6_theta_min = - M_PI_2;
+#define _AR_M_DH_rl_j6_theta_max = M_PI_2;
+#define _AR_M_DH_rl_j6_mass = 0.0; // 179.8308000;
+#define _AR_M_DH_rl_j6_com_x = 0.0; // 2.8558632;
+#define _AR_M_DH_rl_j6_com_y = 0.0; // -17.3958130;
+#define _AR_M_DH_rl_j6_com_z = 0.0; // -0.1439557;
+
+// Join 7 (fixed)
+#define _AR_M_DH_rl_j7_a = 0.0;
+#define _AR_M_DH_rl_j7_d = 0.0;
+#define _AR_M_DH_rl_j7_alpha = 0.0;
+#define _AR_M_DH_rl_j7_theta_off = M_PI_2;
+
+// Mass properties of the torso
+#define _AR_M_TOP_com_x = -5.5616935;
+#define _AR_M_TOP_com_y = -0.26180284;
+#define _AR_M_TOP_com_z = 111.41225;
+#define _AR_M_TOP_com_mass = 1879.0305;
+
+// Convex hull of the foot of 7th joint
+#define _AR_M_FT_CH_x_min = -28.5;
+#define _AR_M_FT_CH_x_max = 98.5;
+#define _AR_M_FT_CH_y_min = -27.0;
+#define _AR_M_FT_CH_y_max = 27.0;
+
+#endif
